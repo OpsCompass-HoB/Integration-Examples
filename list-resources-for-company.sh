8f7e9be5b5770aca75f764ceb883f0bc2e769ab5
oc-beste <145719927+oc-beste@users.noreply.github.com>
2023-11-21 13:50:38 -0500

    Adding list-resources-for-company.sh

---
+++ list-resources-for-company.sh
@@ -0,0 +1,24 @@
+## Set the company name here
+company_name="xxx"
+
+resource_counter=0
+
+while true; do
+  resource_list=$(opscompass resources list --company "$company_name" --skip "$resource_counter" | jq -c '.[]')
+
+  # Check to see if we have any more resources to process
+  if [ -z "$resource_list" ]; then
+    echo "Received an empty array. Exiting."
+    break
+  fi
+
+
+  # Iterate over each JSON object
+  while IFS= read -r resource; do
+    ## Increment this for each resource we get
+    ((resource_counter++))
+    echo "Processing JSON object #$resource_counter"
+    echo "Resource ID: $(echo "$resource" | jq -r '.id')"
+  done <<< "$resource_list"
+
+done
